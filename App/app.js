127ac9ed5385c0a987f11caae9b144b4d8a5a9bf
Andrey Kuznetsov <and.rey.q@yandex.ru>
2021-04-03 19:53:52 +0300

    Update structure

--- App/app.js
+++ App/app.js
@@ -1,6 +1,26 @@
-const http = require('http');
-const server = http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!');
+const log = console.log
+
+const chalk = require('chalk');
+const mysql = require('mysql2');
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(__dirname + 'public'));
+
+app.use(express.json({ limit: '1mb'}));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {   
+    res.render("index");
 });
-server.listen(3000);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => log(`Server running... \n` + chalk.black.bgGreen(`http://localhost:${PORT}/`)));
+
+// const http = require('http');
+// const server = http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello, World!');
+// });
+// server.listen(3000);
