1b2a65b1ffe4c74afc86654da4f457ac73d9c6aa
Andrey Kuznetsov <and.rey.q@yandex.ru>
2021-04-04 21:20:48 +0300

    Add React

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const log = console.log
 
+const path = require('path')
+
 const chalk = require('chalk');
 const mysql = require('mysql2');
 const express = require("express");
@@ -9,17 +11,15 @@ require('dotenv/config');
 const app = express();
 
 if (process.env.NODE_ENV === "local") {
-    app.use(express.static(__dirname + './../public_html'));
+    app.use(express.static(__dirname + './../public_html/build'));
 } else {
-    app.use(express.static(__dirname + 'public'));
+    app.use(express.static(__dirname))
+    app.use(express.static(path.resolve(__dirname, 'build')))
 }
 
-app.use(express.json({ limit: '1mb'}));
-app.set("view engine", "pug");
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
-app.get("/", (req, res) => {   
-    res.render("index");
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => log(`Server running... \n` + chalk.black.bgGreen(`http://localhost:${PORT}/`)));
