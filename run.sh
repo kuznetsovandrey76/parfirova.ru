210199aacdc394634f8b0dce61488be53209d14b
Andrey Kuznetsov <and.rey.q@yandex.ru>
2021-04-14 10:55:28 +0300

    Update run script - add comments

--- run.sh
+++ run.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+echo -e 'Pull changes from GitHub\n'
 git pull
+
+echo -e 'Install dependencies in root directory\n'
 yarn
+
+echo -e 'Install dependencies in server directory\n'
 cd server/
 npm i
+
+echo -e 'Install dependencies in frontend directory\n'
 cd ../public_html
 yarn
+
+echo -e 'Build frontend\n'
 yarn build
+
+echo -e 'Restart server\n'
 cd ../server
 touch tmp/restart.txt
