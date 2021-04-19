8628273583942befc06b29320e8d0172c8c0cfdb
andrey <and.rey.q@yandex.ru>
2021-04-19 07:42:29 +0300

    Update links

--- public_html/src/assets/photos/index.js
+++ public_html/src/assets/photos/index.js
@@ -1,6 +1,16 @@
-function linkToPhoto() {
-  //   return `https://raw.githubusercontent.com/kuznetsovandrey76/parfirova.ru/master/public_html/src/assets/files/${file}?token=${token}`;
-  return 'https://sprutio.beget.com/image_cache/yoda.beget:9443/akuznetsov/66a67c43c134ae65f30added0bafe22c/10.jpg';
+function linkToPhoto(hash, file) {
+  return `https://sprutio.beget.com/image_cache/yoda.beget:9443/akuznetsov/${hash}/${file}`;
 }
 
-export const photos = [{ src: linkToPhoto() }];
+export const photos = [
+  { src: linkToPhoto('fd08c294c46d406d4cc08eca11b6c939', '1.jpg') },
+  { src: linkToPhoto('3166e6d26850f88ed54b261d181d4f6a', '2.jpg') },
+  { src: linkToPhoto('3782ee2008471511ad011434d24e3ed9', '3.jpg') },
+  { src: linkToPhoto('7c364658d272bbecf85bfc345145b2d7', '4.jpg') },
+  { src: linkToPhoto('77ce75368f5691f323dbf9732b0901b9', '5.jpg') },
+  { src: linkToPhoto('eaacabd7a794fc47ecfdcd29bbeef4a2', '6.jpg') },
+  { src: linkToPhoto('4d43726bf603091abaf2d47d196c64a4', '7.jpg') },
+  { src: linkToPhoto('15d75d89fef92b57cc14802e9e1a5194', '8.jpg') },
+  { src: linkToPhoto('45b1048ca643ca6b95717021a5ccf1b8', '9.jpg') },
+  { src: linkToPhoto('66a67c43c134ae65f30added0bafe22c', '10.jpg') },
+];
