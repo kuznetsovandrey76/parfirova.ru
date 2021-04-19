ecd5f94b778f1cb0473978b911832170d3ed5822
andrey <and.rey.q@yandex.ru>
2021-04-19 10:15:09 +0300

    Upload images to s3

--- public_html/src/assets/photos/index.js
+++ public_html/src/assets/photos/index.js
@@ -1,22 +1,22 @@
-function linkToPhoto(hash, file) {
-  return `https://sprutio.beget.com/image_cache/yoda.beget:9443/akuznetsov/${hash}/${file}`;
+function linkToPhoto(file) {
+  return `https://parfirova.s3.eu-west-3.amazonaws.com/images/gallery/${file}`;
 }
 
 export const photos = [
-  { src: linkToPhoto('fd08c294c46d406d4cc08eca11b6c939', '1.jpg') },
-  { src: linkToPhoto('3166e6d26850f88ed54b261d181d4f6a', '2.jpg') },
-  { src: linkToPhoto('3782ee2008471511ad011434d24e3ed9', '3.jpg') },
-  { src: linkToPhoto('7c364658d272bbecf85bfc345145b2d7', '4.jpg') },
-  { src: linkToPhoto('77ce75368f5691f323dbf9732b0901b9', '5.jpg') },
-  { src: linkToPhoto('eaacabd7a794fc47ecfdcd29bbeef4a2', '6.jpg') },
-  { src: linkToPhoto('4d43726bf603091abaf2d47d196c64a4', '7.jpg') },
-  { src: linkToPhoto('15d75d89fef92b57cc14802e9e1a5194', '8.jpg') },
-  { src: linkToPhoto('45b1048ca643ca6b95717021a5ccf1b8', '9.jpg') },
-  { src: linkToPhoto('66a67c43c134ae65f30added0bafe22c', '10.jpg') },
-  { src: linkToPhoto('6ca807004e7517265f3925d9c004732c', '11.jpg') },
-  { src: linkToPhoto('4161bb87cf4a9f086906773abba946d8', '12.jpg') },
-  { src: linkToPhoto('84c948069a24a18a9880671bc126be4e', '13.jpg') },
-  { src: linkToPhoto('2ca3233b2a0c8ca94dc78033a3171eea', '14.jpg') },
-  { src: linkToPhoto('d23e906a3494cb5718b6b4129acf50f2', '15.jpg') },
-  { src: linkToPhoto('e42f3917fc216cec46c89c72f73208cc', '16.jpg') },
+  { src: linkToPhoto('1.jpg'), caption: 'Мы за ЗОЖ! 5 класс' },
+  { src: linkToPhoto('2.jpg'), caption: 'Волковский театр. 5 класс' },
+  { src: linkToPhoto('3.jpg'), caption: 'Новогоднее поздравление. 6 класс' },
+  { src: linkToPhoto('4.jpg'), caption: '7 класс' },
+  { src: linkToPhoto('5.jpg'), caption: 'Учитель года.' },
+  { src: linkToPhoto('6.jpg'), caption: 'Поездка в Карабиху.' },
+  { src: linkToPhoto('7.jpg'), caption: 'Лосиная ферма. Кострома.' },
+  { src: linkToPhoto('8.jpg'), caption: 'Новогодняя Москва.' },
+  { src: linkToPhoto('9.jpg'), caption: 'Спортивный класс. Соревнования.' },
+  { src: linkToPhoto('12.jpg'), caption: 'Новогодняя представление.' },
+  { src: linkToPhoto('13.jpg'), caption: 'Конкурс "Дружба народов". Греция' },
+  { src: linkToPhoto('14.jpg'), caption: 'К нам пришли Снегурочка с Дедом Морозом.' },
+  { src: linkToPhoto('15.jpg'), caption: 'КВН.' },
+  { src: linkToPhoto('16.jpg'), caption: 'Карабиха.' },
+  { src: linkToPhoto('17.jpg'), caption: 'КВН.' },
+  { src: linkToPhoto('11.jpg'), caption: 'Зарница.' },
 ];
