72e08ea3c70fe850d985c22577034783ddc8bf1d
Andrey Kuznetsov <and.rey.q@yandex.ru>
2022-03-31 15:44:14 +0300

    Add ant design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 function App() {
   const url = 'https://parfirova.herokuapp.com/';
+  const [collapsed, setCollapsed] = useState(false);
 
+  const onCollapse = (collapsed) => {
+    setCollapsed(collapsed);
+  };
   useEffect(() => {
     axios.get(url).then((resp) => {
       const { data } = resp;
@@ -23,7 +38,46 @@ function App() {
       });
   }, []);
 
-  return <div className='App'>Hello, World!</div>;
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <div className='logo' />
+        <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
+          <Menu.Item key='1' icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key='2' icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key='sub1' icon={<UserOutlined />} title='User'>
+            <Menu.Item key='3'>Tom</Menu.Item>
+            <Menu.Item key='4'>Bill</Menu.Item>
+            <Menu.Item key='5'>Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key='sub2' icon={<TeamOutlined />} title='Team'>
+            <Menu.Item key='6'>Team 1</Menu.Item>
+            <Menu.Item key='8'>Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key='9' icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className='site-layout'>
+        <Header className='site-layout-background' style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Пользователь</Breadcrumb.Item>
+            <Breadcrumb.Item>Ирина</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className='site-layout-background' style={{ padding: 24, minHeight: 360 }}>
+            Ирина Парфирова
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
+  );
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,62 @@
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: path.resolve(__dirname, './src/index.js'),
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
-        test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader', // Automatically prefix
+            options: {
+              plugins: [require('autoprefixer')({ browsers: ['last 5 version'] })],
+            },
+          },
+          { loader: 'less-loader' },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+          { loader: 'postcss-loader' },
+        ],
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, "./build"),
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
     }),
     new MiniCssExtractPlugin(),
     new webpack.HotModuleReplacementPlugin(),
   ],
   devServer: {
-    static: path.resolve(__dirname, "./"),
+    static: path.resolve(__dirname, './'),
     port: 8081,
     hot: true,
   },
