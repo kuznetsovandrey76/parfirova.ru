de1768e9342b399edaa461da2fc85fbdac3b94ce
Andrey Kuznetsov <and.rey.q@yandex.ru>
2022-04-08 09:53:46 +0300

    Add markdown

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const api = axios.create({
-    withCredentials: true,
-    baseURL: process.env.API_URL,
+  withCredentials: true,
+  baseURL: process.env.API_URL,
 });
 
 export default class {
-    static getHome = () => api.get('/');
-    static getCourses = () => api.get('courses/');
-    static getGallery = () => api.get('gallery/');
+  static getHome = () => api.get('/');
+  static getCourses = () => api.get('courses/');
+  static getGallery = () => api.get('gallery/');
+  static getPosts = () => api.get('posts/');
 
-    static signIn = (login, pass) => api.post('login/', { login, pass })
+  static signIn = (login, pass) => api.post('login/', { login, pass });
 }
--- src/components/pages/5grade.js
+++ src/components/pages/5grade.js
@@ -1,27 +1,58 @@
-import React from 'react';
-import { WordSvg, ExcelSvg, PpointSvg } from '../../assets/svg';
-
+import React, { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import { Container } from 'react-bootstrap';
 
-function FiveGradePage() {
+import { WordSvg, ExcelSvg, PpointSvg } from '../../assets/svg';
+import Api from '../../api';
 
-    return (
-        <Container fluid className='mt-2 mb-5 text-center'>
-            <p>5 класс</p>
-            <a href='#' className='d-block mb-2'>
-                <img src={WordSvg} className='me-2' style={{ width: '30px' }} />
-                <span>Урок 1</span>
-            </a>
-            <a href='#' className='d-block mb-2'>
-                <img src={ExcelSvg} className='me-2' style={{ width: '30px' }} />
-                <span>Урок 2</span>
-            </a>
-            <a href='#' className='d-block mb-2'>
-                <img src={PpointSvg} className='me-2' style={{ width: '30px' }} />
-                <span>Урок 3</span>
-            </a>
-        </Container>
-    );
+function FiveGradePage() {
+  const [posts, setPosts] = useState([]);
+  useEffect(async () => {
+    try {
+      const response = await Api.getPosts();
+      const { data } = response;
+      setPosts(data);
+    } catch (err) {
+      console.warn('Cannot get images from server');
+    }
+  }, []);
+
+  return (
+    <Container fluid className='mt-2 mb-5 text-center'>
+      {posts.map((post, id) => {
+        console.log(JSON.stringify(post.text.replace(/\n/g, '')));
+        const md = `A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+- [ ] todo
+- [x] done
+
+A table:
+
+| a | b |
+| - | - |
+`;
+        // return <ReactMarkdown key={id}>{post.text.replace(/\n/g, '').trim()}</ReactMarkdown>;
+        return <ReactMarkdown children={md} remarkPlugins={[remarkGfm]} key={id} />;
+      })}
+      <p>5 класс</p>
+      <a href='#' className='d-block mb-2'>
+        <img src={WordSvg} className='me-2' style={{ width: '30px' }} />
+        <span>Урок 1</span>
+      </a>
+      <a href='#' className='d-block mb-2'>
+        <img src={ExcelSvg} className='me-2' style={{ width: '30px' }} />
+        <span>Урок 2</span>
+      </a>
+      <a href='#' className='d-block mb-2'>
+        <img src={PpointSvg} className='me-2' style={{ width: '30px' }} />
+        <span>Урок 3</span>
+      </a>
+    </Container>
+  );
 }
 
 export default FiveGradePage;
