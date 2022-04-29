ba02a817eeca7ec227df01530dde876be7bd1bf1
Andrey Kuznetsov <and.rey.q@yandex.ru>
2022-04-29 11:27:20 +0300

    Add spinner to third-school page

--- src/components/pages/third-school.js
+++ src/components/pages/third-school.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, createRef } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Spinner } from 'react-bootstrap';
 import Lightbox from 'react-image-lightbox';
 import api from '../../api';
 import { getNod } from '../utils';
@@ -9,44 +9,51 @@ function ThirdSchoolPage() {
   const [isOpen, setIsOpen] = useState(false);
   const [photoIndex, setPhotoIndex] = useState(0);
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(async () => {
     try {
-      const response = await api.getThirdSchool();
-      const { data: images } = response;
-
-      const imagePromises = images.map((image, idx) => {
-        return new Promise((resolve, reject) => {
-          const img = new Image();
-          img.onload = function () {
-            const nod = getNod(this.width, this.height);
-            resolve({
-              ...image,
-              src: image.location,
-              width: this.width / nod,
-              height: this.height / nod,
-              idx,
-            });
-          };
-          img.onerror = () => {
-            resolve({
-              src: 'https://storage.yandexcloud.net/parfirova.ru/const/not-found.jpg',
-              width: 3,
-              height: 2,
-              idx,
-            });
-          };
-          img.src = image.location;
-        });
-      });
-
-      const transformImages = await Promise.all(imagePromises);
-
-      setImages(transformImages);
+      await getImages();
     } catch (err) {
       console.warn('Cannot get images from server');
+    } finally {
+      // todo: Add progress bar
+      setIsLoading(false);
     }
   }, []);
+
+  async function getImages() {
+    const response = await api.getThirdSchool();
+    const { data: images } = response;
+
+    const imagePromises = images.map((image, idx) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = function () {
+          const nod = getNod(this.width, this.height);
+          resolve({
+            ...image,
+            src: image.location,
+            width: this.width / nod,
+            height: this.height / nod,
+            idx,
+          });
+        };
+        img.onerror = () => {
+          resolve({
+            src: 'https://storage.yandexcloud.net/parfirova.ru/const/not-found.jpg',
+            width: 3,
+            height: 2,
+            idx,
+          });
+        };
+        img.src = image.location;
+      });
+    });
+
+    const transformImages = await Promise.all(imagePromises);
+    setImages(transformImages);
+  }
 
   const imagesBlock =
     isOpen && images.length ? (
@@ -65,15 +72,21 @@ function ThirdSchoolPage() {
   return (
     <Container fluid className='mt-2 mb-5 text-center'>
       <h2>3 школа, Ростов Великий</h2>
-      <Gallery
-        photos={images}
-        direction={'row'}
-        onClick={(e) => {
-          setPhotoIndex(e.target.attributes.idx.value);
-          setIsOpen(true);
-        }}
-      />
-      {imagesBlock}
+      {isLoading ? (
+        <Spinner animation='border' variant='danger' />
+      ) : (
+        <>
+          <Gallery
+            photos={images}
+            direction={'row'}
+            onClick={(e) => {
+              setPhotoIndex(e.target.attributes.idx.value);
+              setIsOpen(true);
+            }}
+          />
+          {imagesBlock}
+        </>
+      )}
     </Container>
   );
 }
