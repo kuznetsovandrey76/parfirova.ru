97665c560437aa69c5995484497f2a50f429e9f5
Andrey Kuznetsov <and.rey.q@yandex.ru>
2022-05-03 20:52:51 +0300

    Add horizontal scrolling

--- src/components/pages/5th-grade.js
+++ src/components/pages/5th-grade.js
@@ -1,14 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Card, Row, Col, Spinner } from 'react-bootstrap';
+
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { LeftArrow, RightArrow } from '../shared/arrows';
+import Card2 from '../shared/card';
+import useDrag from '../shared/useDrag';
+
 import Switch from 'react-switch';
 
 import api from '../../api';
+
+import './5th-grade.css';
+
+const elemPrefix = 'test';
+const getId = (index) => `${elemPrefix}${index}`;
+
+const getItems = () =>
+  Array(20)
+    .fill(0)
+    .map((_, ind) => ({ id: getId(ind) }));
 
 function FiveGradePage() {
   const [isLoading, setIsLoading] = useState(false);
   const [lessons, setLessons] = useState([]);
   const [checked, setChecked] = useState(false);
   const [currentLessons, setCurrentLessons] = useState([]);
+
+  const [items] = React.useState(getItems);
+
+  // NOTE: for drag by mouse
+  const { dragStart, dragStop, dragMove, dragging } = useDrag();
+  const handleDrag =
+    ({ scrollContainer }) =>
+    (ev) =>
+      dragMove(ev, (posDiff) => {
+        if (scrollContainer.current) {
+          scrollContainer.current.scrollLeft += posDiff;
+        }
+      });
+
+  const [selected, setSelected] = React.useState('');
+  const handleItemClick = (itemId) => {
+    if (dragging) {
+      return false;
+    }
+    setSelected(selected !== itemId ? itemId : '');
+  };
 
   useEffect(async () => {
     try {
@@ -54,8 +91,42 @@ function FiveGradePage() {
     </Row>
   );
 
+  console.log({ items });
   return (
     <Container fluid className='mt-2 mb-5'>
+      <div onMouseLeave={dragStop} className='mb-3'>
+        <ScrollMenu
+          LeftArrow={LeftArrow}
+          RightArrow={RightArrow}
+          // onWheel={onWheel}
+          onMouseDown={() => dragStart}
+          onMouseUp={() => dragStop}
+          onMouseMove={handleDrag}
+        >
+          {items.map(({ id }) => (
+            // <Card
+            //   title={id}
+            //   itemId={id} // NOTE: itemId is required for track items
+            //   key={id}
+            //   onClick={() => handleItemClick(id)}
+            //   selected={id === selected}
+            // >
+            //   <Card.Body>
+            //     <Card.Title>Тема: {id}</Card.Title>
+            //     {/* <Card.Subtitle>{lesson.subject}</Card.Subtitle> */}
+            //     {/* <Card.Text>{lesson.description}</Card.Text> */}
+            //   </Card.Body>
+            // </Card>
+            <Card2
+              title={id}
+              itemId={id} // NOTE: itemId is required for track items
+              key={id}
+              onClick={() => handleItemClick(id)}
+              selected={id === selected}
+            />
+          ))}
+        </ScrollMenu>
+      </div>
       <div className='d-flex mb-3'>
         <h1 className='d-inline-block me-4'>5 класс</h1>
         <Switch
@@ -112,6 +183,21 @@ function FiveGradePage() {
       {isLoading ? <Spinner animation='border' variant='danger' /> : lessonsBlock}
     </Container>
   );
+}
+
+function onWheel(apiObj, ev) {
+  const isThouchpad = Math.abs(ev.deltaX) !== 0 || Math.abs(ev.deltaY) < 15;
+
+  if (isThouchpad) {
+    ev.stopPropagation();
+    return;
+  }
+
+  if (ev.deltaY < 0) {
+    apiObj.scrollNext();
+  } else if (ev.deltaY > 0) {
+    apiObj.scrollPrev();
+  }
 }
 
 export default FiveGradePage;
